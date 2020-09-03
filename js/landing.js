6fcdb63df35550bd71293b77bd0a7fa20cb6d75b
unknown <kavitaanand528@gmail.com>
2020-09-03 12:55:51 +0530

    first

---
+++ js/landing.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#epic-services1").owlCarousel();
+    $("#epic-services2").owlCarousel();
+    $("#epic-services3").owlCarousel();
+    $("#epic-services4").owlCarousel();
+    $("#epic-services5").owlCarousel();
+});
